33e283a46894b16844f922dcd01cc066a6651f09
Oscar255321 <oabemal0810@g.educaand.es>
2025-03-07 09:24:11 +0100

    Tercer commit Recuperación Tarea 6.1 - En este tercer comit realizo un cambio en la documentación del modelo Paperformat.

--- src/main/java/com/example/interfazconodoo/modelos/Paperformat.java
+++ src/main/java/com/example/interfazconodoo/modelos/Paperformat.java
@@ -13,7 +13,11 @@ import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
 
 /**
- * Clase en la que tenemos los campos que encontramos en la tabla "report_paperformat".
+ * Clase que representa la estructura de la tabla "report_paperformat" en la base de datos.
+ * Contiene propiedades con sus respectivos métodos de acceso y modificación.
+ *
+ * <p>Esta clase utiliza propiedades de JavaFX para facilitar la
+ * vinculación con la interfaz gráfica.</p>
  *
  * @author Oscar Abellan
  * @version 1.0
@@ -48,7 +52,16 @@ public class Paperformat {
   private final DoubleProperty marginRight = new SimpleDoubleProperty(this, "marginRight");
 
   /**
-   * Constructor con los parametros que se van a usar.
+   * Constructor con parámetros.
+   *
+   * @param id  Identificador del formato.
+   * @param name  Nombre del formato.
+   * @param format  Formato de la página (Ejemplo: A4, Letter, etc.).
+   * @param orientation  Orientación del formato (Portrait o Landscape).
+   * @param marginTop  Margen superior.
+   * @param marginBottom  Margen inferior.
+   * @param marginLeft  Margen izquierdo.
+   * @param marginRight  Margen derecho.
    */
 
   public Paperformat(Integer id, String name, String format, String orientation, Double marginTop,
@@ -70,22 +83,24 @@ public class Paperformat {
   public Paperformat() {
   }
 
-  /**
-   * Obtener el id del formato.
-   *
-   * @return El id del formato.
-   */
-
   public IntegerProperty idProperty() {
     return id;
   }
+
+  /**
+   * Obtiene el identificador del formato.
+   *
+   * @return ID del formato.
+   */
 
   public int getId() {
     return id.get();
   }
 
   /**
-   * Insertar el id del formato.
+   * Establece el identificador del formato.
+   *
+   * @param id Nuevo ID del formato.
    */
 
   public void setId(int id) {
@@ -117,144 +132,196 @@ public class Paperformat {
   }
 
   public IntegerProperty headerSpacingProperty() {
+
     return headerSpacing;
   }
 
   public int getHeaderSpacing() {
+
     return headerSpacing.get();
   }
 
   public void setHeaderSpacing(int headerSpacing) {
+
     this.headerSpacing.set(headerSpacing);
   }
 
   public IntegerProperty dpiProperty() {
+
     return dpi;
   }
 
   public int getDpi() {
+
     return dpi.get();
   }
 
   public void setDpi(int dpi) {
+
     this.dpi.set(dpi);
   }
 
   public IntegerProperty createUidProperty() {
+
     return createUid;
   }
 
   public int getCreateUid() {
+
     return createUid.get();
   }
 
   public void setCreateUid(int createUid) {
+
     this.createUid.set(createUid);
   }
 
   public IntegerProperty writeUidProperty() {
+
     return writeUid;
   }
 
   public int getWriteUid() {
+
     return writeUid.get();
   }
 
   public void setWriteUid(int writeUid) {
+
     this.writeUid.set(writeUid);
   }
 
   /**
-   * Obtener el nombre del formato.
+   * Obtiene la propiedad name, útil para enlazarla con UI en JavaFX.
    *
-   * @return El nombre del formato.
+   * @return Propiedad del nombre del formato.
    */
 
   public StringProperty nameProperty() {
+
     return name;
   }
 
+  /**
+   * Obtiene el nombre del formato.
+   *
+   * @return Nombre del formato.
+   */
+
   public String getName() {
+
     return name.get();
   }
 
   /**
-   * Insertar el nombre del formato.
+   * Establece el nombre del formato.
+   *
+   * @param name Nuevo nombre del formato.
    */
 
   public void setName(String name) {
+
     this.name.set(name);
   }
 
   /**
-   * Obtener el formato del formato.
+   * Obtiene la propiedad del formato del papel.
    *
-   * @return El formato del formato.
+   * @return Propiedad del formato.
    */
 
   public StringProperty formatProperty() {
+
     return format;
   }
 
+  /**
+   * Obtiene el tipo de formato de la página (Ejemplo: A4, Letter, etc.).
+   *
+   * @return Formato del papel.
+   */
+
   public String getFormat() {
+
     return format.get();
   }
 
   /**
-   * Inseratr el nombre del formato.
+   * Establece el tipo de formato del papel.
+   *
+   * @param format Nuevo tipo de formato del papel.
    */
 
   public void setFormat(String format) {
+
     this.format.set(format);
   }
 
   /**
-   * Obtener la orientación del formato.
+   * Obtiene la propiedad de la orientación del papel.
    *
-   * @return La orientación del formato.
+   * @return Propiedad de la orientación.
    */
 
   public StringProperty orientationProperty() {
+
     return orientation;
   }
 
+  /**
+   * Obtiene la orientación del papel (Portrait o Landscape).
+   *
+   * @return Orientación del formato.
+   */
+
   public String getOrientation() {
+
     return orientation.get();
   }
 
   /**
-   * Insertar la orientación del formato.
+   * Establece la orientación del papel.
+   *
+   * @param orientation Nueva orientación del formato.
    */
 
   public void setOrientation(String orientation) {
+
     this.orientation.set(orientation);
   }
 
   public BooleanProperty defaultProperty() {
+
     return defecto;
   }
 
   public boolean isDefault() {
+
     return defecto.get();
   }
 
   public void setDefault(boolean defecto) {
+
     this.defecto.set(defecto);
   }
 
   public BooleanProperty headerLineProperty() {
+
     return headerLine;
   }
 
   public boolean isHeaderLine() {
+
     return headerLine.get();
   }
 
   public void setHeaderLine(boolean headerLine) {
+
     this.headerLine.set(headerLine);
   }
 
   public BooleanProperty disableShrinkingProperty() {
+
     return disableShrinking;
   }
 
@@ -267,114 +334,164 @@ public class Paperformat {
   }
 
   public ObjectProperty<Timestamp> createDateProperty() {
+
     return createDate;
   }
 
   public Timestamp getCreateDate() {
+
     return createDate.get();
   }
 
   public void setCreateDate(Timestamp createDate) {
+
     this.createDate.set(createDate);
   }
 
   public ObjectProperty<Timestamp> writeDateProperty() {
+
     return writeDate;
   }
 
   public Timestamp getWriteDate() {
+
     return writeDate.get();
   }
 
   public void setWriteDate(Timestamp writeDate) {
+
     this.writeDate.set(writeDate);
   }
 
   /**
-   * Obtener el margen superior del formato.
+   * Obtiene la propiedad del margen superior.
    *
-   * @return El margen superior del formato.
+   * @return Propiedad del margen superior.
    */
 
   public DoubleProperty marginTopProperty() {
+
     return marginTop;
   }
 
+  /**
+   * Obtiene el margen superior.
+   *
+   * @return Margen superior en unidades de medida de la base de datos.
+   */
+
   public double getMarginTop() {
+
     return marginTop.get();
   }
 
   /**
-   * Inserta el margen superior del formato.
+   * Establece el margen superior.
+   *
+   * @param marginTop Nuevo valor para el margen superior.
    */
 
   public void setMarginTop(double marginTop) {
+
     this.marginTop.set(marginTop);
   }
 
   /**
-   * Obtener el margen inferior del formato.
+   * Obtiene la propiedad del margen inferior.
    *
-   * @return El margen inferior del formato.
+   * @return Propiedad del margen inferior.
    */
 
   public DoubleProperty marginBottomProperty() {
+
     return marginBottom;
   }
 
+  /**
+   * Obtiene el margen inferior.
+   *
+   * @return Margen inferior en unidades de medida de la base de datos.
+   */
+
   public double getMarginBottom() {
+
     return marginBottom.get();
   }
 
   /**
-   * Insertar el margen inferior del formato.
+   * Establece el margen inferior.
+   *
+   * @param marginBottom Nuevo valor para el margen inferior.
    */
 
   public void setMarginBottom(double marginBottom) {
+
     this.marginBottom.set(marginBottom);
   }
 
   /**
-   * Obtener el margen izquierdo del formato.
+   * Obtiene la propiedad del margen izquierdo.
    *
-   * @return El margen izquierdo del formato.
+   * @return Propiedad del margen izquierdo.
    */
 
   public DoubleProperty marginLeftProperty() {
+
     return marginLeft;
   }
 
+  /**
+   * Obtiene el margen izquierdo.
+   *
+   * @return Margen izquierdo en unidades de medida de la base de datos.
+   */
+
   public double getMarginLeft() {
+
     return marginLeft.get();
   }
 
   /**
-   * Insertar el margen izquierdo del formato.
+   * Establece el margen izquierdo.
+   *
+   * @param marginLeft Nuevo valor para el margen izquierdo.
    */
 
   public void setMarginLeft(double marginLeft) {
+
     this.marginLeft.set(marginLeft);
   }
 
   /**
-   * Obtener el margen derecho del formato.
+   * Obtiene la propiedad del margen derecho.
    *
-   * @return El margen derecho del formato.
+   * @return Propiedad del margen derecho.
    */
 
   public DoubleProperty marginRightProperty() {
+
     return marginRight;
   }
 
+  /**
+   * Obtiene el margen derecho.
+   *
+   * @return Margen derecho en unidades de medida de la base de datos.
+   */
+
   public double getMarginRight() {
+
     return marginRight.get();
   }
 
   /**
-   * Insertar el margen derecho del formato.
+   * Establece el margen derecho.
+   *
+   * @param marginRight Nuevo valor para el margen derecho.
    */
 
   public void setMarginRight(double marginRight) {
+
     this.marginRight.set(marginRight);
   }
 
