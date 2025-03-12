da804b61fcef29900531913a7a8deef2bd1aa926
Oscar255321 <oabemal0810@g.educaand.es>
2025-03-12 13:42:25 +0100

    Cuarto commit Recuperacion Tarea 8.2 - En este commit realizo las ultimas modificaciones y finallizo todos los test.

--- src/test/java/com/example/test/com/example/interfazconodoo/TestRecuperacion.java
+++ src/test/java/com/example/test/com/example/interfazconodoo/TestRecuperacion.java
@@ -1,9 +1,11 @@
 package com.example.interfazconodoo;
 
+import com.lowagie.text.Row;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -12,6 +14,8 @@ import org.testfx.api.FxRobot;
 import org.testfx.framework.junit5.ApplicationExtension;
 import org.testfx.framework.junit5.Start;
 import org.testfx.matcher.base.WindowMatchers;
+import org.testfx.matcher.control.TableViewMatchers;
+import org.testfx.matcher.control.TextInputControlMatchers;
 
 /**
  * Author: Usuario
@@ -51,17 +55,30 @@ class TestRecuperacion {
     robot.targetWindow("Crear Formato");
     robot.sleep(1000);
 
-    robot.clickOn("#textoname").write("CartaNueva");
+    robot.clickOn("#textoname").write("Nueva");
+    FxAssert.verifyThat("#textoname", TextInputControlMatchers.hasText("Nueva"));
     robot.clickOn("#textoformat").write("A4");
+    FxAssert.verifyThat("#textoformat", TextInputControlMatchers.hasText("A4"));
     robot.clickOn("#textorientation").write("Portrait");
+    FxAssert.verifyThat("#textorientation", TextInputControlMatchers.hasText("Portrait"));
     robot.clickOn("#margint").write("10");
+    FxAssert.verifyThat("#margint", TextInputControlMatchers.hasText("10"));
     robot.clickOn("#marginb").write("10");
+    FxAssert.verifyThat("#marginb", TextInputControlMatchers.hasText("10"));
     robot.clickOn("#marginl").write("10");
+    FxAssert.verifyThat("#marginl", TextInputControlMatchers.hasText("10"));
     robot.clickOn("#marginr").write("10");
+    FxAssert.verifyThat("#marginr", TextInputControlMatchers.hasText("10"));
 
     robot.clickOn("#btcrear");
     robot.sleep(1000);
     robot.closeCurrentWindow();
+    robot.sleep(1000);
+    robot.clickOn("#textobuscar").write("Nueva");
+    robot.clickOn("#btBuscar");
+    robot.sleep(2000);
+    FxAssert.verifyThat("#tbDatos", tableView -> tableView.isVisible() && !tableView.getId().isEmpty());
+
   }
 
   @DisplayName("Realizamos la edición de un elemento")
@@ -79,25 +96,35 @@ class TestRecuperacion {
     robot.sleep(1000);
 
     robot.doubleClickOn("#textoname").eraseText(10).write("Carta_Editada");
+    FxAssert.verifyThat("#textoname", TextInputControlMatchers.hasText("Carta_Editada"));
     robot.doubleClickOn("#textoformat").eraseText(10).write("A3");
+    FxAssert.verifyThat("#textoformat", TextInputControlMatchers.hasText("A3"));
     robot.doubleClickOn("#textorientation").eraseText(10).write("Left");
+    FxAssert.verifyThat("#textorientation", TextInputControlMatchers.hasText("Left"));
     robot.doubleClickOn("#margint").eraseText(2).write("15");
+    FxAssert.verifyThat("#margint", TextInputControlMatchers.hasText("15"));
     robot.doubleClickOn("#marginb").eraseText(2).write("15");
+    FxAssert.verifyThat("#marginb", TextInputControlMatchers.hasText("15"));
     robot.doubleClickOn("#marginl").eraseText(2).write("8");
+    FxAssert.verifyThat("#marginl", TextInputControlMatchers.hasText("8"));
     robot.doubleClickOn("#marginr").eraseText(2).write("8");
+    FxAssert.verifyThat("#marginr", TextInputControlMatchers.hasText("8"));
 
     robot.clickOn("#btcrear");
     robot.sleep(1000);
-    robot.targetWindow("Confirmación");
-    robot.sleep(1000);
-    robot.moveTo("Aceptar").clickOn();
-    robot.sleep(500);
+    robot.closeCurrentWindow();
+    robot.doubleClickOn("#textobuscar").write("Carta_Editada");
+    robot.clickOn("#btBuscar");
+    robot.sleep(2000);
+    FxAssert.verifyThat("#tbDatos", tableView -> tableView.isVisible() && !tableView.getId().isEmpty());
+
   }
 
   @DisplayName("Realizamos la eliminación de un elemento")
   @Test
   void testBotonEliminarFormato(FxRobot robot) {
-    robot.clickOn("#textobuscar").write("CartaNueva");
+
+    robot.clickOn("#textobuscar").write("Nueva");
     robot.clickOn("#btBuscar");
     robot.sleep(2000);
     FxAssert.verifyThat("#tbDatos", tableView -> tableView.isVisible() && !tableView.getId().isEmpty());
@@ -108,7 +135,8 @@ class TestRecuperacion {
     robot.sleep(1000);
     robot.moveTo("Aceptar").clickOn();
     robot.sleep(2000);
-    FxAssert.verifyThat("#tbDatos", );
+    Assertions.assertTrue(robot.lookup("#tbDatos").queryTableView().getItems().isEmpty());
+
   }
 
   @DisplayName("Cerramos la ventana secundaria")
