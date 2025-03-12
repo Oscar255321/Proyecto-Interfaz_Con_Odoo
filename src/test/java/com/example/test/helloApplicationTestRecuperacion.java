ed63f0d24ea0fd1e4a291765b933931a27e948bd
Oscar255321 <oabemal0810@g.educaand.es>
2025-03-12 09:10:00 +0100

    Primer commit Recuperacion Tarea 8.2 - En este commit realizo los primeros cambios implementando algunos verifythat.

--- src/test/java/com/example/test/helloApplicationTestRecuperacion.java
+++ src/test/java/com/example/test/helloApplicationTestRecuperacion.java
@@ -1,8 +1,4 @@
-package com.example.interfazconodoo;
-
-
-import static org.testfx.api.FxAssert.verifyThat;
-import static org.testfx.matcher.control.TextMatchers.hasText;
+package com.example.test;
 
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -18,10 +14,10 @@ import org.testfx.framework.junit5.Start;
 import org.testfx.matcher.base.WindowMatchers;
 
 /**
- * Author: Usuario Date: 12/02/2025 Description:
+ * Author: Usuario Date: 11/03/2025 Description:
  */
 @ExtendWith(ApplicationExtension.class)
-class HelloApplicationTests {
+class helloApplicationTestRecuperacion {
 
   Pane mainroot;
   Stage mainstage;
@@ -35,37 +31,24 @@ class HelloApplicationTests {
     stage.toFront();
   }
 
-  /**
-   * En este test lo que hacemos es realizar la busqueda de un elemento en la inteerfaz.
-   * @param robot
-   */
-
-  @DisplayName("Realizamos la busqueda de un elemento")
-
+  @DisplayName("Realizamos la búsqueda de un elemento")
   @Test
   void testBotonBuscar(FxRobot robot) {
     robot.clickOn("#textobuscar").write("Carta");
     robot.clickOn("#btBuscar");
     robot.sleep(2000);
-    verifyThat("#tbDatos", tableView -> !tableView.getId().isEmpty());
+    FxAssert.verifyThat("#tbDatos", tableView -> tableView.isVisible() && !tableView.getId().isEmpty());
   }
 
-  /**
-   * En este test lo que hacemos es realizar la creacion de un elemento en la inteerfaz.
-   * @param robot
-   */
-
-  @DisplayName("Realizamos la cracion de un elemento")
-
+  @DisplayName("Realizamos la creación de un elemento")
   @Test
   void testBotonCrearFormato(FxRobot robot) {
-
     robot.clickOn("#btCrear");
     robot.sleep(1000);
     FxAssert.verifyThat(robot.window("Crear Formato"), WindowMatchers.isShowing());
     robot.targetWindow("Crear Formato");
-    //FxAssert.verifyThat("#tfTitulo", LabeledMatchers.hasText("Nuevo formato"));
     robot.sleep(1000);
+
     robot.clickOn("#textoname").write("CartaNueva");
     robot.clickOn("#textoformat").write("A4");
     robot.clickOn("#textorientation").write("Portrait");
@@ -73,97 +56,62 @@ class HelloApplicationTests {
     robot.clickOn("#marginb").write("10");
     robot.clickOn("#marginl").write("10");
     robot.clickOn("#marginr").write("10");
+
     robot.clickOn("#btcrear");
     robot.sleep(1000);
     robot.closeCurrentWindow();
-
   }
 
-  /**
-   * En este test lo que hacemos es realizar la edicion de un elemento en la inteerfaz.
-   * @param robot
-   */
-
-  @DisplayName("Realizamos la edicion de un elemento")
-
+  @DisplayName("Realizamos la edición de un elemento")
   @Test
   void testBotonEditarFormato(FxRobot robot) {
-
-    //Buscamos el formato que queremos modificar.
     robot.clickOn("#textobuscar").write("Carta");
     robot.clickOn("#btBuscar");
     robot.sleep(2000);
-    verifyThat("#tbDatos", tableView -> !tableView.getId().isEmpty());
+    FxAssert.verifyThat("#tbDatos", tableView -> tableView.isVisible() && !tableView.getId().isEmpty());
 
-    //Lo seleccionamos en la tabla
     robot.clickOn("#tbDatos .table-row-cell");
     robot.clickOn("#btEditar");
-    //Se abre la nueva ventana
     FxAssert.verifyThat(robot.window("Modificar Formato"), WindowMatchers.isShowing());
     robot.targetWindow("Modificar Formato");
     robot.sleep(1000);
 
-    //Introducimos los datos modificados
-    robot.doubleClickOn("#textoname").eraseText(1).write("Carta_Editada");
-    robot.sleep(500);
-    robot.doubleClickOn("#textoformat").eraseText(1).write("A3");
-    robot.sleep(500);
-    robot.doubleClickOn("#textorientation").eraseText(1).write("Left");
-    robot.sleep(500);
-    robot.doubleClickOn("#margint").eraseText(1).write("15");
-    robot.sleep(500);
-    robot.doubleClickOn("#marginb").eraseText(1).write("15");
-    robot.sleep(500);
-    robot.doubleClickOn("#marginl").eraseText(1).write("8");
-    robot.sleep(500);
-    robot.doubleClickOn("#marginr").eraseText(1).write("8");
-    robot.sleep(500);
+    robot.doubleClickOn("#textoname").eraseText(10).write("Carta_Editada");
+    robot.doubleClickOn("#textoformat").eraseText(10).write("A3");
+    robot.doubleClickOn("#textorientation").eraseText(10).write("Left");
+    robot.doubleClickOn("#margint").eraseText(2).write("15");
+    robot.doubleClickOn("#marginb").eraseText(2).write("15");
+    robot.doubleClickOn("#marginl").eraseText(2).write("8");
+    robot.doubleClickOn("#marginr").eraseText(2).write("8");
+
     robot.clickOn("#btcrear");
     robot.sleep(1000);
     robot.targetWindow("Confirmación");
     robot.sleep(1000);
-    robot.moveTo("Aceptar");
-    robot.clickOn();
+    robot.moveTo("Aceptar").clickOn();
     robot.sleep(500);
-
   }
 
-  /**
-   * En este test lo que hacemos es realizar la eliminacion de un elemento en la inteerfaz.
-   * @param robot
-   */
-
-  @DisplayName("Realizamos la eliminacion de un elemento")
-
+  @DisplayName("Realizamos la eliminación de un elemento")
   @Test
   void testBotonEliminarFormato(FxRobot robot) {
-
-    //Buscamos el formato que queremos modificar.
     robot.clickOn("#textobuscar").write("CartaNueva");
     robot.clickOn("#btBuscar");
     robot.sleep(2000);
-    verifyThat("#tbDatos", tableView -> !tableView.getId().isEmpty());
+    FxAssert.verifyThat("#tbDatos", tableView -> tableView.isVisible() && !tableView.getId().isEmpty());
 
     robot.clickOn("#tbDatos .table-row-cell");
     robot.clickOn("#btEliminar");
-    robot.targetWindow("ADVERTENCIA!");
+    FxAssert.verifyThat(robot.window("ADVERTENCIA!"), WindowMatchers.isShowing());
     robot.sleep(1000);
-    robot.moveTo("Aceptar");
-    robot.clickOn();
+    robot.moveTo("Aceptar").clickOn();
     robot.sleep(2000);
-
+    FxAssert.verifyThat("#tbDatos", tableView -> tableView.isVisible() && tableView.getId().isEmpty());
   }
 
-  /**
-   * En este test lo que hacemos es verificar que se cierra la ventana en la inteerfaz.
-   * @param robot
-   */
-
   @DisplayName("Cerramos la ventana secundaria")
-
   @Test
-  void testBotonCerrarVentana(FxRobot robot){
-
+  void testBotonCerrarVentana(FxRobot robot) {
     robot.clickOn("#btCrear");
     robot.sleep(1000);
     FxAssert.verifyThat(robot.window("Crear Formato"), WindowMatchers.isShowing());
@@ -171,6 +119,7 @@ class HelloApplicationTests {
     robot.sleep(1000);
     robot.clickOn("#btsalir");
     robot.sleep(1000);
-
+    FxAssert.verifyThat(robot.window("Crear Formato"), WindowMatchers.isNotShowing());
   }
 }
+
